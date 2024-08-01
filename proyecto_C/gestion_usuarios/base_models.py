fafd27ee98e48e7a360331592237e007d58daaeb
EssPollo <alan.david_pollo@hotmail.com>
2024-08-01 12:00:14 -0600

    Creacion del modelo Usuario

---
+++ proyecto_C/gestion_usuarios/base_models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Persona(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField()
+
+
+    class Meta:
+        abstract = True
